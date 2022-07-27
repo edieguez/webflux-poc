ef84a40c9fc6b4b778ed737f7c385c203dcf6411
edieguez <dieguezemmanuel@gmail.com>
2022-07-26 23:29:47 -0400

    Added extra properties to Repository

--- src/main/java/com/artemisa/webfluxpoc/model/Repository.java
+++ src/main/java/com/artemisa/webfluxpoc/model/Repository.java
@@ -1,10 +1,19 @@
 package com.artemisa.webfluxpoc.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class Repository {
+
+    private String name;
     private String full_name;
+    private String html_url;
+
+    @JsonProperty("private")
+    private boolean private_;
+
+    private boolean fork;
 }
