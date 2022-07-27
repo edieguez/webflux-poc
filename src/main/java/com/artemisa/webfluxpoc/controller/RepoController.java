1b8ebc4dce07ac445d2665d9c038d5b90a1cc8f2
edieguez <dieguezemmanuel@gmail.com>
2022-07-26 22:33:21 -0400

    Added Lombok

--- src/main/java/com/artemisa/webfluxpoc/controller/RepoController.java
+++ src/main/java/com/artemisa/webfluxpoc/controller/RepoController.java
@@ -1,15 +1,17 @@
 package com.artemisa.webfluxpoc.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class RepoController {
 
     @GetMapping("/{username}/repos")
     public void getRepositories(@PathVariable String username) {
-        System.out.println("Getting repositories for " + username);
+        log.info("Getting repositories for {}", username);
     }
 
 }
